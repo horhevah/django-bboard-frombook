9fc9cf905f35b4cc649e1ef82a45c6fe60084c66
horhevah <horhevah@mail.ru>
2020-11-27 11:47:55 +0300

    Init repo

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import index
+from .views import other_page
+from .views import BBLoginView
+from .views import profile
+from .views import BBLogoutView
+app_name = 'main'
+urlpatterns = [
+	path('accounts/logout/',BBLogoutView.as_view(), name='logout'),
+	path('accounts/profile/',profile, name='profile'),
+	path('accoutns/login/', BBLoginView.as_view(), name='login'),
+	path('<str:page>',other_page,name='other'),
+	path('', index, name = 'index'),
+
+]
---
+++ main/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+#from django.shortcuts import get_object_or_404
+
+from .models import AdvUser
+from .forms import ChangeUserInfoForm
+
+# Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+def other_page(request,page):
+	try:
+		template = get_template('main/' + page + '.html')
+	except TemplateDoesNotExist:
+		raise Http404
+	return HttpResponse(template.render(request = request))
+
+class BBLoginView(LoginView):
+	template_name = 'main/login.html'
+
+@login_required
+def profile(request):
+	return render(request, 'main/profile.html')
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+	template_name='main/logout.html'
+
+class ChangeUserInfoView (SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+	model = AdvUser
+	template_name = 'main/change_user_info.html'
+	form_class = ChangeUserInfoForm
+	success_url = reverse_lazy('main:profile')
+	success_message = 'Личные данные пользователя изменены'
+
+	def dispatch(self, request, *arg, **kwargs):
+		self.user_id = request.user.pk
+		return super().dispatch(request,*arg, **kwargs)
+
+	def get_object(self, queryset=None):
+		if not queryset:
+			queryset = self.get_queryset()
+		return get_object_or_404(queryset, pk=self.user_id)
+
+
+			
