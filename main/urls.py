d671ce09ef57a5bf8e415433932cf17e92a249e3
horhevah <horhevah@mail.ru>
2020-12-01 16:01:42 +0300

    Acnivation by MAIL WORK!

--- main/urls.py
+++ main/urls.py
@@ -7,8 +7,11 @@ from .views import BBLogoutView
 from .views import ChangeUserInfoView
 from .views import BBPasswordChangeView
 from .views import RegisterUserView, RegisterDoneView
+from .views import user_activate
+
 app_name = 'main'
 urlpatterns = [
+	path('accounts/register/activate/<str:sign>/',user_activate, name='register_activate'),
 	path('accounts/register/',RegisterUserView.as_view(),name = 'register'),
 	path('accounts/register/done/',RegisterDoneView.as_view(),name = 'register_done'),
 	path('accounts/password/change/', BBPasswordChangeView.as_view(),name='password_change'),
